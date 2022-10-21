5e34c048658c480eefb6a70060061cdcd3159955
IgnaSebas <sebastian.igna.kb@gmail.com>
2022-10-21 11:23:38 +0200

    NavBar Logic

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,17 +1,78 @@
-import React, { useState } from 'react';
+import React , {useState} from 'react';
+import { Link } from 'react-router-dom'
 import '../assets/css/App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = ({ props }) => {
+    const [sidebar, setSidebar] = useState(true);
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
         <>
+        {sidebar ? (
             <div className='sidebar'>
-                <div className='sidebar_section'>
-                    <FontAwesomeIcon icon={faHouse}/>
-                    <h5>Hola</h5>
+                <div onClick={showSidebar} className='sidebar_section'>
+                    <div className="sidebar_icon">
+                        <FontAwesomeIcon icon={faHouse}/>
+                    </div>
+                    <h5 className="sidebar_text">Cerrar</h5>
                 </div>
-            </div>          
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <div className='sidebar_section'>
+                        <div className="sidebar_icon">
+                            <FontAwesomeIcon icon={faHouse}/>
+                        </div>
+                        <h5 className="sidebar_text">Home</h5>
+                    </div>
+                </Link>
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <div className='sidebar_section'>
+                        <div className="sidebar_icon">
+                            <FontAwesomeIcon icon={faHouse}/>
+                        </div>
+                        <h5 className="sidebar_text">Clientes</h5>
+                    </div>
+                </Link>
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <div className='sidebar_section'>
+                        <div className="sidebar_icon">
+                            <FontAwesomeIcon icon={faHouse}/>
+                        </div>
+                        <h5 className="sidebar_text">Pedidos</h5>
+                    </div>
+                </Link>
+            </div> 
+        ):(
+            <div className='sidebar_close'>
+                <div onClick={showSidebar} className='sidebar_section_close'>
+                    <div className="sidebar_icon">
+                        <FontAwesomeIcon icon={faHouse}/>
+                    </div>
+                </div>
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <div className='sidebar_section_close'>
+                        <div className="sidebar_icon">
+                            <FontAwesomeIcon icon={faHouse}/>
+                        </div>
+                    </div>
+                </Link>
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <div className='sidebar_section_close'>
+                        <div className="sidebar_icon">
+                            <FontAwesomeIcon icon={faHouse}/>
+                        </div>
+                    </div>
+                </Link>
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <div className='sidebar_section_close'>
+                        <div className="sidebar_icon">
+                            <FontAwesomeIcon icon={faHouse}/>
+                        </div>
+                    </div>
+                </Link>
+            </div> 
+        )}         
         </>
     );
 };
