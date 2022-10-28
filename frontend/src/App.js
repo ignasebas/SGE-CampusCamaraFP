55daacf4aabeb594e0ca811eb8fe9abce9906fe5
IgnaSebas <sebastian.igna.kb@gmail.com>
2022-10-28 10:45:22 +0200

    New Ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import Routes from './Routes';
+import './App.css';
 
-import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import ForgetPasswordPage from './pages/ForgetPasswordPage'
-import HomePage from './pages/HomePage'
-
-import './assets/css/App.css'
-
-export default function App() {
-    return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route exact path="/" element={ <LandingPage/> } />
-                    <Route path="/login" element={ <LoginPage/> } />
-                    <Route path="/register" element={ <RegisterPage/> } />
-                    <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
-                    <Route path="/home" element={ <HomePage/> } />
-                </Routes>
-            </div>
-        </Router>
-    )
+function App() {
+	
+  return (
+    <div>
+    	<Routes/>
+    </div>
+  );
 }
 
-const FooterStyle = {
-    background: "#222",
-    fontSize: ".8rem",
-    color: "#fff",
-    position: "absolute",
-    bottom: 0,
-    padding: "1rem",
-    margin: 0,
-    width: "100%",
-    opacity: ".5"
-}
+export default App;
