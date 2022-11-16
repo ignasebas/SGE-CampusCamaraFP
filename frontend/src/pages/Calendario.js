139e73ca28e7803e45b858a701c2c65e943296f1
RaulRecolons <97520477+RaulRecolons@users.noreply.github.com>
2022-11-16 08:20:55 +0100

    Calendario v1
    No se que estoy haciendo uwu

--- frontend/src/pages/Calendario.js
+++ frontend/src/pages/Calendario.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 import { CDBBtn, CDBIframe, CDBView } from "cdbreact";
 import Sidebar from "../Sidebar";
+import Calendar,{onClickDay} from 'react-calendar';
 import Navbar from "../Navbar";
+import 'react-calendar/dist/Calendar.css';
 import "./Profile.css"
 
 export const Calendario = () => {
-
+	const [value, onChange] = useState(new Date());
+	function formulario(){
+		return <h2>hola</h2>
+	}
+	
 	return (
 		<div className="d-flex profile">
 			<div>
@@ -15,9 +21,14 @@ export const Calendario = () => {
 				<Navbar/>
 				<div style={{height:"100%"}}>
 							<div style={{height:"calc(100% - 64px)", padding:"20px 5%", overflowY:"scroll"}}>
-								<div style={{margin:"0 auto", maxWidth:"1320px"}}>
-									<div  className="cards-container1">
-										<div>
+								<div>
+		  							<Calendar onChange={onChange} 
+									value={value}
+									onClickDay={formulario}/>
+									
+								</div>
+
+								<div>
 											<div className="card shadow border-0">
 												<img alt="profileImage" src="/img/pages/heroImage2.png" className="w-100" style={{objectFit:"cover", maxHeight:"500px"}}/>
 												<div className="card-body">
@@ -33,114 +44,10 @@ export const Calendario = () => {
 												</div>
 											</div>
 										</div>
-										<div className="mini-container">
-											<div>
-									<div className="card shadow border-0">
-										<img
-										alt="cardImg"
-										className="img-fluid"
-										style={{objectFit:"cover"}}
-										src="/img/cardImage.png"
-										/>
-										<div className="p-3">
-										<h3>Basic</h3>
-										<p>This is just a card text Get important notifications about you or activity you've missed </p>
-										<CDBBtn style={{background:"#333", border:"none"}}>
-											Button
-											</CDBBtn>
-										</div>
-									</div>
-											</div>
-											<div>
-									<div className="card shadow border-0 h-75 mx-auto" style={{backgroundImage:"url('https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_960_720.jpg')"}}>
-										<div className="p-3 d-flex flex-column h-100 w-100">
-										<h4 className="mt-3 text-white">Heading</h4>
-										<p className="text-white">Paragraph</p>
-										<div className="d-flex justify-content-center mt-auto">
-											<CDBBtn color="light" flat circle>
-											Button
-											</CDBBtn>
-										</div>
-										</div>
-									</div>
-								</div>
-								</div>
-									</div>
-									<div className="cards-container2">
-										<div>
-								<div className="card shadow border-0">
-									<img
-									alt="cardImg"
-									className="img-fluid"
-									style={{objectFit:"cover"}}
-									src="/img/cardImage.png"
-									/>
-									<img alt="cardImg" className="mx-auto border rounded-circle" style={{marginTop:"-5rem"}} width="130px" src="/img/pane/pane4.png" />
-									<div className="p-3 d-flex flex-column align-items-center mb-4 text-center">
-									<h4 style={{fontWeight:"600"}}>Sammy Russo</h4>
-									<p>Senior Software Developer</p>
-									<p className="text-muted">Detroit, USA</p>
-									<div className="d-flex justify-content-center flex-wrap">
-										<CDBBtn className="mr-2" size="small" color="dark"><i className="fas fa-user-plus"></i> Connect</CDBBtn>
-										<CDBBtn size="small" color="warning"> Send Message </CDBBtn>
-									</div>
-									</div>
-								</div>
-										</div>
-										<div>
-											<div className="card shadow border-0">
-												<div>
-													<img src="/img/pages/promotionImage2.png" alt="Project" className="img-fluid"/>
-												</div>
-												<div className="card-body">
-													<h4 className="card-title mb-3">
-														<span style={{fontWeight:"600"}}>Project Name</span>
-													</h4>
-													<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas.</p>
-												</div>
-												<div className="card-footer">
-													<a className="p-2" href="#profile">
-														Live Preview
-														<i className="far fa-image ml-1"></i>
-													</a>
-												</div>
-											</div>
-										</div>
-										<div>
-											<div className="card shadow border-0">
-												<div>
-													<img src="/img/pages/promotionImage.png" alt="Project" className="img-fluid"/>
-												</div>
-												<div className="card-body">
-													<h4 className="card-title text-center mb-3" style={{fontWeight:"600"}}>
-														Card Title
-													</h4>
-													<p className="card-text text-center ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas.</p>
-												</div>
-												<div className="card-footer text-center">
-													<a className="p-2" href="#profile">Live Preview<i className="far fa-image ml-1"></i></a>
-												</div>
-											</div>
-										</div>
-										<div>
-								<div className="card shadow border-0">
-									<div className="p-3">
-									<h4 className="mt-3">Heading</h4>
-									<p>Paragraph</p>
-									</div>
-									<CDBView>
-									<CDBIframe src="https://www.youtube.com/embed/xnczyP2jSR0"></CDBIframe>
-									</CDBView>
-									<div className="p-3">
-									<CDBBtn color="dark" flat outline circle>
-										Button
-									</CDBBtn>
-									</div>
-								</div>
-										</div>
-									</div>
+							<div style={{margin:"0 auto", maxWidth:"1320px"}}>
+									
+								
 							<footer className="d-flex mx-auto py-4">
-							<small className="mx-auto my-1 text-center">&copy; Devwares, 2020. All rights reserved.</small>
 							</footer>
 							</div>
 							</div>
