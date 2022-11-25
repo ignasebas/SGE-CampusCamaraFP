d31022f160d036f4594381debdfcdccf547dfa97
Anthony <71320520+DSAnthony@users.noreply.github.com>
2022-11-25 10:36:34 +0100

    Profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { CDBBtn, CDBIframe, CDBView } from "cdbreact";
 import Sidebar from "../Sidebar";
 import Navbar from "../Navbar";
 import "./Profile.css"
@@ -17,28 +16,40 @@ export const Profile = () => {
 					<div style={{ height: "calc(100% - 64px)", padding: "20px 5%", overflowY: "scroll" }}>
 
 						{/*EDITAR A PARTIR DE AQUÍ*/}
-						<div style={{ margin: "0 auto", maxWidth: "1320px" }}>
+						<div style={{ margin: "0 auto", maxWidth: "1320px"}}>
 							<div className="cards-container1">
-								<div>
+								<form>
+									<div className="card shadow border-0" >
+										<label for="imagen">Cambiar imagen:</label>
+										<input type="file" name="imagen"></input><br/>
+									</div><br/><br/>
 									<div className="card shadow border-0">
-										<img alt="profileImage" src="/img/pages/heroImage2.png" className="w-100" style={{ objectFit: "cover", maxHeight: "500px" }} />
-										<div className="card-body">
-											<h4 className="card-title mb-2" style={{ fontWeight: "600" }}>Warren Briggs</h4>
-											<h5 className="mb-2">Photographer</h5>
-											<p className="text-justify mt-4">
-												<strong className="mb-2">Desciption: </strong>
-												Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione perferendis quod animi dignissimos
-											</p>
-											<div className="justify-content-end pr-1">
-												<CDBBtn color="dark" outline>More</CDBBtn>
-											</div>
-										</div>
-									</div>
-								</div>
+										<label for="nombre">Cambiar nombre:</label>
+										<input type="text" name="imagen"></input><br />
+
+										<label for="apellidos">Cambiar apellidos:</label>
+										<input type="text" name="imagen"></input><br />
+									</div><br/><br/>
+									<div className="card shadow border-0">
+										<label for="telefono">Cambiar teléfono:</label>
+										<input type="text" name="telefono"></input><br />
+
+										<label for="correo">Cambiar correo:</label>
+										<input type="text" name="correo"></input><br />
+
+										<label for="direccion">Cambiar correo:</label>
+										<input type="text" name="direccion"></input>
+									</div><br/><br/>
+									<div className="card shadow border-0">
+										<input type="submit" value="Actualizar"></input>
+									</div><br/><br/>
+								</form>
 							</div>
-							<footer className="d-flex mx-auto py-4">
-								<small className="mx-auto my-1 text-center">&copy; Devwares, 2020. All rights reserved.</small>
-							</footer>
+							<div style={{margin:"0 auto", maxWidth:"1320px"}}>
+								<footer className="d-flex mx-auto py-4">
+									<small className="mx-auto my-1 text-center">&copy; EMS Tech, 2022. All rights reserved.</small>
+								</footer>
+							</div>
 						</div>
 					</div>
 				</div>
