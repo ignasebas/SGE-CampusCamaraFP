fc066bf4b442ab081134ef0006440e095a63c6bd
Anthony <71320520+DSAnthony@users.noreply.github.com>
2022-11-25 09:21:21 +0100

    Shin Chan

--- frontend/src/pages/Hero404.js
+++ frontend/src/pages/Hero404.js
@@ -16,13 +16,14 @@ export const Hero404 = () => {
         <div style={{ height: "100%" }}>
           <div style={{ height: "calc(100% - 64px)", padding: "20px 5%", overflowY: "scroll" }}>
             {/*EDITAR A PARTIR DE AQUÍ*/}
-            <section className="page-body">
+            <section className="page-body" style={{display:"flex"}}>
               <div className="message404">
                 <h4 className="h1 font-weight-bold">Oops</h4>
                 <h4 className="h3 my-4">Se ha producido un error</h4>
                 <p>Lo sentimos, hmmm... probablemente falta una página o el enlace es incorrecto.</p>
                 <CDBBtn flat color="dark" className="py-2 btn-block">Volver</CDBBtn>
               </div>
+              <img className="image404" alt="404" src="/sad.png" width="200px" />
             </section>
           </div>
         </div>
