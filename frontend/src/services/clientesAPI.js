49400dca3a3341f8580728e39c4b514115d810e9
IrisLuna <97520441+IrisLuna@users.noreply.github.com>
2022-12-16 10:52:08 +0100

    delete?

--- frontend/src/services/clientesAPI.js
+++ frontend/src/services/clientesAPI.js
@@ -12,3 +12,10 @@ export const postCliente = async (data) => {
   const response = await axios.post(apiUrl, data);
   return response.data;
 }
+
+export const deleteCliente = async (id) => {
+    const apiUrl = getApiUrl(`/deleteByIdCliente/${id}`);
+    const response = await axios.delete(apiUrl);
+    return response.data;
+}
+  
