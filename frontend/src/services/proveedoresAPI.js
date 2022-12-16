c6286cb35f541927228262ca0ce8eee4feba4a1e
IgnaSebas <82389120+ignasebas@users.noreply.github.com>
2022-12-16 10:42:54 +0100

    proveedorAPI

--- frontend/src/services/proveedoresAPI.js
+++ frontend/src/services/proveedoresAPI.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 import { getApiUrl } from "./config";
 
-export const getClientes = () => {
-    const ApiUrl = getApiUrl('/allCliente')
+export const getProveedores = () => {
+    const ApiUrl = getApiUrl('/getAllProveedor')
     return axios.get(ApiUrl).then(res => res.data)
+}
+
+export const postProveedores = async (data) => {
+  const apiUrl = getApiUrl('/postProveedor');
+  const response = await axios.post(apiUrl, data);
+  return response.data;
 }
