59e6844d63b8d6bee0e95ebc42cfe7ac689f2855
IgnaSebas <82389120+ignasebas@users.noreply.github.com>
2023-01-23 09:55:27 +0100

    Update AddModal.js

--- frontend/src/components/AddModal.js
+++ frontend/src/components/AddModal.js
@@ -54,8 +54,8 @@ const AddModal = ({handleShow,dataModifier,state,clientes,compras,empleados,prod
 
 		if (ventas) {
 			console.log(state)
-			const { dni, nombreCliente, apellidos, email, direccion, fechaVenta, precioTotal, observaciones, descripcion} = state;
-		  	postVentas({"nif":dni, "nombre":nombreCliente, "apellidos":apellidos, "email":email, "direccion":direccion, "fechaVenta":fechaVenta, "precioVenta":precioTotal, "observaciones":observaciones,"descripcion":descripcion});
+			const { dni, nombre, apellidos, email, direccion, fechaVenta, precioTotal, observaciones, descripcion} = state;
+		  	postVentas({"nif":dni, "nombre":nombre, "apellidos":apellidos, "email":email, "direccion":direccion, "fechaVenta":fechaVenta, "precioTotal":precioTotal, "observaciones":observaciones,"descripcion":descripcion});
 			handleShow()
 		}
 
